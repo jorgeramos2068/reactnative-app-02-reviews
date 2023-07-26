db9c4193848b15139c81ca1134a63bad5b5f967e
Jorge Ramos <jorgeramos2068@gmail.com>
2023-07-25 19:26:37 -0600

    Add functionality to send parameters between screens

--- screens/Home.js
+++ screens/Home.js
@@ -1,19 +1,27 @@
-import React from 'react';
-import { Button, View, Text } from 'react-native';
+import React, { useState } from 'react';
+import { FlatList, View, Text, TouchableOpacity } from 'react-native';
 import globalStyles from '../styles/globalStyles';
 
 const Home = ({navigation}) => {
-  const handlePress = () => {
-    //navigation.navigate('ReviewDetails');
-    navigation.push('ReviewDetails');
+  const [reviews, setReviews] = useState([
+    { key: '1', title: 'Batman Returns', rating: 5, body: 'lorem ipsum' },
+    { key: '2', title: 'Avengers Endgame', rating: 4, body: 'lorem ipsum' },
+    { key: '3', title: 'Spiderman', rating: 3, body: 'lorem ipsum' },
+  ]);
+
+  const handlePress = (item) => {
+    navigation.navigate('ReviewDetails', item);
   };
 
   return (
     <View style={globalStyles.container}>
-      <Text style={globalStyles.titleText}>Home screen</Text>
-      <Button
-        title="Go to Review Details"
-        onPress={handlePress}
+      <FlatList
+        data={reviews}
+        renderItem={({item}) => (
+          <TouchableOpacity onPress={() => handlePress(item)}>
+            <Text style={globalStyles.titleText}>{item.title}</Text>
+          </TouchableOpacity>
+        )}
       />
     </View>
   );
--- screens/ReviewDetails.js
+++ screens/ReviewDetails.js
@@ -1,19 +1,13 @@
 import React from 'react';
-import { Button, Text, View } from 'react-native';
+import { Text, View } from 'react-native';
 import globalStyles from '../styles/globalStyles';
 
 const ReviewDetails = ({navigation}) => {
-  const handlePress = () => {
-    navigation.goBack();
-  };
-
   return (
     <View style={globalStyles.container}>
-      <Text>ReviewDetails screen</Text>
-      <Button
-        title="Back to home screen"
-        onPress={handlePress}
-      />
+      <Text>{navigation.getParam('title')}</Text>
+      <Text>{navigation.getParam('body')}</Text>
+      <Text>{navigation.getParam('rating')}</Text>
     </View>
   );
 };
