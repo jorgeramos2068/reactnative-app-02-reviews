a6dc7749c992ac352b122436bc897cbce0e1fe07
Jorge Ramos <jorgeramos2068@gmail.com>
2023-07-25 19:26:40 -0600

    Add background image on header

--- screens/Home.js
+++ screens/Home.js
@@ -1,25 +1,52 @@
 import React, { useState } from 'react';
-import { FlatList, View, Text, TouchableOpacity } from 'react-native';
+import { FlatList, Modal, View, StyleSheet, Text, TouchableOpacity } from 'react-native';
+import { MaterialIcons } from'@expo/vector-icons';
 import Card from '../shared/Card';
 import { globalStyles } from '../styles/global';
 
 const Home = ({navigation}) => {
+  const [modalOpen, setModalOpen] = useState(false);
   const [reviews, setReviews] = useState([
     { key: '1', title: 'Batman Returns', rating: 5, body: 'lorem ipsum' },
     { key: '2', title: 'Avengers Endgame', rating: 4, body: 'lorem ipsum' },
     { key: '3', title: 'Spiderman', rating: 3, body: 'lorem ipsum' },
   ]);
 
-  const handlePress = (item) => {
+  const handleNavigate = (item) => {
     navigation.navigate('ReviewDetails', item);
+  };
+
+  const handleModalOpen = () => {
+    setModalOpen(true);
+  };
+
+  const handleModalClose = () => {
+    setModalOpen(false);
   };
 
   return (
     <View style={globalStyles.container}>
+      <Modal visible={modalOpen} animationType="slide">
+        <View style={styles.modalContent}>
+          <MaterialIcons
+            name="close"
+            size={24}
+            onPress={handleModalClose}
+            style={{...styles.modalToggle, ...styles.modalClose}}
+          />
+          <Text>Hello from the modal</Text>
+        </View>
+      </Modal>
+      <MaterialIcons
+        name="add"
+        size={24}
+        onPress={handleModalOpen}
+        style={styles.modalToggle}
+      />
       <FlatList
         data={reviews}
         renderItem={({item}) => (
-          <TouchableOpacity onPress={() => handlePress(item)}>
+          <TouchableOpacity onPress={() => handleNavigate(item)}>
             <Card>
               <Text style={globalStyles.titleText}>{item.title}</Text>
             </Card>
@@ -29,5 +56,24 @@ const Home = ({navigation}) => {
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  modalClose: {
+    marginBottom: 0,
+    marginTop: 20,
+  },
+  modalContent: {
+    flex: 1,
+    marginTop: 24
+  },
+  modalToggle: {
+    alignSelf: 'center',
+    borderColor: '#f2f2f2',
+    borderRadius: 10,
+    borderWidth: 1,
+    marginBottom: 10,
+    padding: 10
+  },
+});
 
 export default Home;
--- shared/Header.js
+++ shared/Header.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Image, ImageBackground, StyleSheet, Text, View } from 'react-native';
 import { MaterialIcons } from'@expo/vector-icons';
 
 const Header = ({navigation, title}) => {
@@ -8,14 +8,15 @@ const Header = ({navigation, title}) => {
   };
 
   return (
-    <View style={styles.header}>
+    <ImageBackground source={require('../assets/images/game_bg.png')} style={styles.header}>
       <MaterialIcons name="menu" size={28} onPress={handleOpenMenu} style={styles.icon} />
-      <View>
+      <View style={styles.headerTitle}>
+        <Image source={require('../assets/images/heart_logo.png')} style={styles.headerImage} />
         <Text style={styles.headerText}>
           {title}
         </Text>
       </View>
-    </View>
+    </ImageBackground>
   );
 };
 
@@ -23,15 +24,23 @@ const styles = StyleSheet.create({
   header: {
     alignItems: 'center',
     flexDirection: 'row',
-    height: '100%',
+    height: 60,
     justifyContent: 'center',
-    width: '100%'
+    width: '100%',
+  },
+  headerImage: {
+    height: 26,
+    marginHorizontal: 10,
+    width: 26
   },
   headerText: {
     color: '#333',
     fontSize: 20,
     fontWeight: 'bold',
     letterSpacing: 1
+  },
+  headerTitle: {
+    flexDirection: 'row',
   },
   icon: {
     left: 16,
